66633ede63988714e243d778a37be40c5c16f6da
Recep Gumus <recepgumusyasin@gmail.com>
2023-04-04 22:50:49 +0300

    footer update

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -4,10 +4,11 @@
       <div class="row mb-5">
         <div class="col-lg-3 py-3">
           <h3>Health Services in <span class="text-primary">Turkey</span></h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero amet, repellendus eius blanditiis in
-            iusto eligendi iure.</p>
+          <p>We are committed to providing you with accurate, reliable, and up-to-date information on medical treatments, hospitals, doctors, accommodation, and transportation in Turkey.
+            If you have any questions or feedback, please do not hesitate to contact us.
+            Our team of professionals is always here to assist you. We wish you a successful and pleasant medical journey in Turkey.</p>
 
-          <div class="social-media-button">
+          <div class="social-media-button" v-if="false">
             <a href="#"><span class="mai-logo-facebook-f"></span></a>
             <a href="#"><span class="mai-logo-twitter"></span></a>
             <a href="#"><span class="mai-logo-google-plus-g"></span></a>
@@ -18,18 +19,15 @@
         <div class="col-lg-3 py-3">
           <h5>Company</h5>
           <ul class="footer-menu">
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Career</a></li>
-            <li><a href="#">Advertise</a></li>
+            <li><NuxtLink to="/about">About Us</NuxtLink></li>
             <li><a href="#">Terms of Service</a></li>
             <li><a href="#">Help & Support</a></li>
           </ul>
         </div>
         <div class="col-lg-3 py-3">
           <h5>Contact Us</h5>
-          <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
           <a href="#" class="footer-link">+00 1122 3344 5566</a>
-          <a href="#" class="footer-link">seogram@temporary.com</a>
+          <a href="#" class="footer-link">info@healthservicesinturkey.com</a>
         </div>
         <div class="col-lg-3 py-3">
           <h5>Newsletter</h5>
@@ -41,8 +39,7 @@
         </div>
       </div>
 
-      <p class="text-center" id="copyright">Copyright &copy; 2020. This template design and develop by <a
-        href="https://macodeid.com/" target="_blank">MACode ID</a></p>
+      <p class="text-center" id="copyright">Copyright &copy; 2023. Health Services in Turkey</p>
     </div>
   </footer>
 </template>
